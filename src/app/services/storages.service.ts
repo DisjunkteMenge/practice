024410f6e68fb0535e2e01c8044af4b9e39d5b6f
DisjunkteMenge <elies.mabrouki@gmail.com>
2019-04-02 19:25:59 +0200

    interface for Item

--- src/app/services/storages.service.ts
+++ src/app/services/storages.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
+export interface Item {
+  id: number;
+  title: string;
+  value: string;
+  modified: number;
+}
+
+
+
+
+
+
+
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
